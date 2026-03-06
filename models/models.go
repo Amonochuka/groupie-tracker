899a888be6b84b545aff9b5c8cdc29ad676bcf94
aochuka <ochukaamon@gmail.com>
2026-03-06 16:19:13 +0300

    feat:add relations model

--- models/models.go
+++ models/models.go
@@ -30,3 +30,7 @@ type Relation struct {
 	ID             int                 `json:"id"`
 	DatesLocations map[string][]string `json:"datesLocations"`
 }
+
+type RelationResponse struct{
+	Index []Relation `json:"index"`
+}
