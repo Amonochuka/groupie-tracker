f8ed7e1bbed59aab99c4e8cee30cab772481cc9f
aochuka <ochukaamon@gmail.com>
2026-03-06 14:42:52 +0300

    feat:final models with their fetch functions

--- api/fetchDates.go
+++ api/fetchDates.go
@@ -1,17 +1,33 @@
 package api
 
 import (
+	"errors"
 	"groupie-tracker/models"
 
-	"net/http"
 	"encoding/json"
+	"net/http"
 )
-func GetDates()([]models.Date,error){
-	resp,err:=http.Get("https://groupietrackers.herokuapp.com/api/dates")
-if err != nil{
-	return nil ,err
+
+func GetDates() ([]models.Date, error) {
+	resp, err := http.Get("https://groupietrackers.herokuapp.com/api/dates")
+	if err != nil {
+		return nil, err
+	}
+	var data models.DateResponse
+	err = json.NewDecoder(resp.Body).Decode(&data)
+	return data.Index, err
 }
-var data models.DateResponse
-err=json.NewDecoder(resp.Body).Decode(&data)
-return data.Index, err
+
+func getDateByID(ID int) (*models.Date, error) {
+	dates, err := GetDates()
+	if err != nil {
+		return nil, err
+	}
+
+	for i := range dates {
+		if dates[i].ID == ID {
+			return &dates[i], nil
+		}
+	}
+	return nil, errors.New(("date not found"))
 }
