43d3140d4e8095e2c17afc8eef6cc04a85ab8d6f
aochuka <ochukaamon@gmail.com>
2026-03-06 16:20:08 +0300

    feat:boilerplate handler code for artists

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"html/template"
 	"net/http"
+	"strconv"
 
 	"groupie-tracker/api"
 	"groupie-tracker/models"
@@ -39,4 +40,26 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		http.Error(w, "Execute error", 500)
 	}
+}
+
+
+func ArtistHandler(w http.ResponseWriter, r *http.Request){
+	idStr := r.URL.Query().Get("id")
+
+	id, err := strconv.Atoi(idStr)
+	if err != nil{
+		http.Error(w, "invalid artist ID", 400)
+		return
+	}
+
+	artist, err := api.FetchArtistByID(id)
+	if err != nil{
+		http.Error(w, "artist not found", 404)
+	}
+
+	tmpl, err := template.ParseFiles()
+	if err!= nil{
+		http.Error(w, "template error", 500)
+	}
+	tmpl.Execute(w, artist)
 }
