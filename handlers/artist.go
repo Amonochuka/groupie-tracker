00f3c0484525f2e330f0bffc1cc23d0b172466cd
sjarso <stephenjacob815@gmail.com>
2026-03-10 10:21:30 +0300

    Feat:Added search functionality to home

--- handlers/artist.go
+++ handlers/artist.go
@@ -4,36 +4,64 @@ import (
 	"html/template"
 	"net/http"
 	"strconv"
+
 	// "strings"
 
 	"groupie-tracker/api"
-	// "groupie-tracker/models"
 )
 
-// type PageData struct {
-// 	Artists []models.Artist
-// 	Dates   []models.Date
-// }
 
-func ArtistHandler(w http.ResponseWriter, r *http.Request){
+
+func ArtistHandler(w http.ResponseWriter, r *http.Request) {
 	idStr := r.URL.Query().Get("id")
 
 	id, err := strconv.Atoi(idStr)
-	if err != nil{
+	if err != nil {
 		http.Error(w, "invalid artist ID", 400)
 		return
 	}
 
 	artist, err := api.FetchArtistByID(id)
-	// relations,_:=api.FetchRelations()
-	
-	if err != nil{
-		http.Error(w, "artist not found", 404)
+	if err != nil {
+		http.Error(w, "Artists error", 500)
+		return
+	}
+
+	relations, err := api.FetchRelations()
+	if err != nil {
+		http.Error(w, "Relations error", 500)
+		return
+	}
+	var rel map[string][]string
+	for _, r := range relations {
+		if r.ID == id {
+			rel = r.DatesLocations
+			break
+		}
+	}
+	data := struct {
+		ID           int
+		Name         string
+		Image        string
+		Members      []string
+		CreationDate int
+		FirstAlbum   string
+		Relations    map[string][]string
+	}{
+		ID:           artist.ID,
+		Name:         artist.Name,
+		Image:        artist.Image,
+		Members:      artist.Members,
+		CreationDate: artist.CreationDate,
+		FirstAlbum:   artist.FirstAlbum,
+		Relations:    rel,
 	}
 
 	tmpl, err := template.ParseFiles("templates/artist.html")
-	if err!= nil{
+	if err != nil {
 		http.Error(w, "template error", 500)
 	}
-	tmpl.Execute(w, artist)
+	if err := tmpl.Execute(w, data); err != nil {
+		http.Error(w, err.Error(), 500)
+	}
 }
--- handlers/home.go
+++ handlers/home.go
@@ -3,6 +3,8 @@ package handlers
 import (
 	"html/template"
 	"net/http"
+	"strings"
+
 	// "strconv"
 	// "strings"
 
@@ -10,41 +12,62 @@ import (
 	"groupie-tracker/models"
 )
 
-type PageData struct {
-	Artists []models.Artist
-	Dates   []models.Date
+type ArtistView struct {
+	models.Artist
+	Relations map[string][]string
 }
 
 // HomeHandler returns an http.HandlerFunc that uses the provided artists
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	// query:=strings.ToLower(r.URL.Query().Get("search"))
+	query := strings.ToLower(r.URL.Query().Get("search"))
 	artists, err := api.FetchArtists()
 	if err != nil {
 		http.Error(w, "Artist error", 500)
 		return
 	}
-// relations ,err:= api.FetchRelations()
-if err != nil{
-	http.Error(w,"RelationsError",500)
-}
-	dates, err := api.GetDates()
+	relations, err := api.FetchRelations()
 	if err != nil {
-		http.Error(w, "Dates error", 500)
-		return
+		http.Error(w, "RelationsError", 500)
+	}
+	// dates, err := api.GetDates()
+	// if err != nil {
+	// 	http.Error(w, "Dates error", 500)
+	// 	return
+	// }
+	var data []ArtistView
+	for _, artist := range artists {
+		if query != "" {
+			match := strings.Contains(strings.ToLower(artist.Name), query)
+			for _, m := range artist.Members {
+				if strings.Contains(strings.ToLower(m), query) {
+					match = true
+				}
+			}
+			if !match {
+				continue
+			}
+		}
+		var rel map[string][]string
+		for _, r := range relations {
+			if r.ID == artist.ID {
+				rel = r.DatesLocations
+				break
+			}
+		}
+
+		data = append(data, ArtistView{
+			Artist:    artist,
+			Relations: rel,
+		})
 	}
 	tmpl, err := template.ParseFiles("templates/index.html")
 	if err != nil {
 		http.Error(w, "Template error", 500)
 		return
 	}
-	data := PageData{
-		Artists: artists,
-		Dates:   dates,
-	}
+
 	err = tmpl.Execute(w, data)
 	if err != nil {
 		http.Error(w, "Execute error", 500)
 	}
 }
-
-
